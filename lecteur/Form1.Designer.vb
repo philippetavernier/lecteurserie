5e8dedcc9bcfaff23fd8dc2925d3e816ddf7c486
cedric <cedric.gaucheron@hotmail.fr>
2013-06-08 10:31:28 +0200

    stocké le contenu de réception du port série dans Excel

---
+++ lecteur/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(390, 22)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(408, 11)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(180, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Connexion au port com"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 56)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Reception"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 85)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(576, 292)
+        Me.RichTextBox1.TabIndex = 3
+        Me.RichTextBox1.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(601, 400)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Lecteur"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+
+End Class
---
+++ lecteur/Form1.vb
@@ -0,0 +1,67 @@
+﻿Imports System.IO.Ports
+Imports Microsoft.Office.Interop
+
+Public Class Form1
+    'exel'
+    Dim xlapp As Excel.Application
+    Dim xlbook As Excel.Workbook
+    Dim xlsheet As Excel.Worksheet
+    Dim x = 1
+    Dim y = 1
+
+    'port'
+    Dim str As String
+    Public WithEvents comPort As SerialPort
+    Public Event ScanDataRecieved(ByVal data As String)
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            comPort = My.Computer.Ports.OpenSerialPort(TextBox1.Text, "19200")
+        Catch ex As Exception
+            MsgBox("Une erreur est survenu", MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+#Region "Reception des données"
+    Private Sub comPort_DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles comPort.DataReceived
+        Try
+            If e.EventType = SerialData.Chars Then
+                Do
+                    Try
+                        Dim message As String = comPort.ReadLine
+                        updateStatus(message)
+                    Catch ex As TimeoutException
+                        updateStatus(ex.ToString)
+                    End Try
+                Loop
+            End If
+            RaiseEvent ScanDataRecieved(str)
+        Catch ex As Exception
+            MsgBox("impossible de lire le flux entrant", MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+    Public Delegate Sub updateStatusDelegate(ByVal newStatus As String)
+    Public Sub updateStatus(ByVal newStatus As String)
+        If Me.InvokeRequired Then
+            Dim upbd As New updateStatusDelegate(AddressOf updateStatus)
+            Me.Invoke(upbd, New Object() {newStatus})
+        Else
+            RichTextBox1.Text = newStatus
+            exel()
+        End If
+    End Sub
+#End Region
+
+    Public Sub exel()
+        xlapp = CType(CreateObject("Excel.Application"), Excel.Application)
+        xlbook = xlapp.Workbooks.Open(Filename:="C:\Users\cedric gaucheron\Documents\Book2.xlsx", Editable:=True, ReadOnly:=False)
+        xlbook.Application.Cells(x, y) = RichTextBox1.Text
+        xlapp.ActiveWorkbook.Save()
+        xlbook.Close()
+        xlapp.Quit()
+        x = x + 1
+        y = y + 1
+        RichTextBox1.Clear()
+    End Sub
+End Class
